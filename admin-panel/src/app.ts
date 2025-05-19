c2141538eafa21dd407e3a13369be2a4758bdcfa
Vicbee57 <joanruth788@gmail.com>
2025-05-19 12:51:09 -0700

    app.ts

--- admin-panel/src/app.ts
+++ admin-panel/src/app.ts
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { setRoutes } from './routes';
-import './index.css';
-
-const App = () => {
-    return (
-        <BrowserRouter>
-            {setRoutes()}
-        </BrowserRouter>
-    );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+{
+  "name": "admin-panel",
+  "version": "1.0.0",
+  "main": "src/app.ts",
+  "scripts": {
+    "start": "ts-node src/app.ts",
+    "build": "tsc"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "sqlite3": "^5.1.6",
+    "body-parser": "^1.20.2",
+    "dotenv": "^16.4.5",
+    "node-fetch": "^2.6.7",
+    "express-session": "^1.17.3"
+  },
+  "devDependencies": {
+    "ts-node": "^10.9.2",
+    "typescript": "^5.4.5"
+  }
+}
